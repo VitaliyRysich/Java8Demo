61f014651ebad93dbc459bb622ec38a40df66221
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2019-12-12 15:55:28 +0100

    An Excepion Handling Approach

---
+++ src/main/java/unit2/ExceptionHandlingExample.java
@@ -0,0 +1,23 @@
+package unit2;
+
+import java.util.function.BiConsumer;
+
+public class ExceptionHandlingExample {
+    public static void main(String[] args) {
+        int numbers[] = {1,2,3,4};
+        int key = 0;
+        process(numbers, key, (v,k) -> {
+            try {
+                System.out.println(v / k);
+            }catch (ArithmeticException e){
+                System.out.println("An ArithmeticExeption happend: " + e);
+            }
+        });
+    }
+
+    private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer){
+        for(int i: someNumbers){
+            consumer.accept(i, key);
+        }
+    }
+}
