44f7a8006483ba479f6e46f95e163dbaa0eb01e6
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-11-11 16:54:32 +0100

    Move exception to wrapper method

--- src/main/java/unit2/ExceptionHandlingExample.java
+++ src/main/java/unit2/ExceptionHandlingExample.java
@@ -6,18 +6,23 @@ public class ExceptionHandlingExample {
     public static void main(String[] args) {
         int numbers[] = {1,2,3,4};
         int key = 0;
-        process(numbers, key, (v,k) -> {
-            try {
-                System.out.println(v / k);
-            }catch (ArithmeticException e){
-                System.out.println("An ArithmeticExeption happend: " + e);
-            }
-        });
+        process(numbers, key, wrapperLambda((v,k) -> System.out.println(v / k)));
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer){
         for(int i: someNumbers){
             consumer.accept(i, key);
         }
+    }
+
+    private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> consumer){
+        return (v, k) -> {
+            try {
+                consumer.accept(v, k);
+            }
+            catch (ArithmeticException e){
+                System.out.println("Exception caught in wrapper lambda");
+            }
+        };
     }
 }
