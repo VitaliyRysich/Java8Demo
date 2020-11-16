f93990de365d2da5641f1663c5f30c144e1f3a31
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-11-16 02:13:53 +0100

    Optionals

---
+++ src/main/java/guide/Optionals/Main.java
@@ -0,0 +1,16 @@
+package guide.Optionals;
+
+import java.util.Optional;
+
+public class Main {
+    //Optional is a simple container for a value which may be null or non-null.
+    public static void main(String[] args) {
+        Optional<String> optional = Optional.of("bam");
+
+        optional.isPresent();           // true
+        optional.get();                 // "bam"
+        optional.orElse("fallback");    // "bam"
+
+        optional.ifPresent((s) -> System.out.println(s.charAt(0)));     // "b"
+    }
+}
